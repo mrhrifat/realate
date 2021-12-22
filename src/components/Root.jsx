a27c319ff8ffec4ed9d67057425e057665cb6dc7
mrhrifat <mrhrifat383759@gmail.com>
2021-12-23 05:28:47 +0600

    Create a class based Root components

---
+++ src/components/Root.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Root extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Bismiallahir Rahmanir Rahim</h3>
+            </div>
+        )
+    }
+}
+
+export default Root
